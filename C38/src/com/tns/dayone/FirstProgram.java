98a32ec182492d0d8a8778c54d8f5cb00619df6c
Administrator <Administrator@ADMIN>
2024-04-21 09:50:09 -0400

    Day One - First Program

---
+++ C38/src/com/tns/dayone/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.tns.dayone;
+
+public class FirstProgram {
+	private void firstProgram() {
+		System.out.println(" Hello World ! ");
+	}
+
+	public static void main(String[] args) {
+		FirstProgram f = new FirstProgram();
+		f.firstProgram();
+	}
+}
